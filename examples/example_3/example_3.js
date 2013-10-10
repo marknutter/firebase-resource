9b175213d0093b7ad8dce3694b871e75e94cc481
Mark Nutter <marknutter@gmail.com>
2013-10-09 19:12:03 -0500

    added some comments to pagination example

--- examples/example_3/example_3.js
+++ examples/example_3/example_3.js
@@ -30,12 +30,20 @@ controller('MainCtrl', function($rootScope, $scope, User, Post) {
   $scope.user.save()
   $scope.current_page = 0;
 
-
+  /*
+   *  Pagination is a way to limit the number of objects coming back
+   *  from Firebase for a given query. It is not intended to only show
+   *  a certain number of objects in a given view; that will still need
+   *  to be handled separatley. So whenever a new page is requested via
+   *  the query() method, those objects are added to the full list of
+   *  objects. For now, pagination is in reverse order, so the most
+   *  recently created objects come back with page 1.
+   */
 
   for (var i=0;i<10;i++) {
     var post = $scope.user.posts().new({content: 'test post ' + i});
     post.save().then(function() {
-      Post.clearAll();
+      Post.clearAll(); // clearing all cached objects so that pagination works for this demo
     });
   }
 
